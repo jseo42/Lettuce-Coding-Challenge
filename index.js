bda8090df7feab4b84092da649bd8ae0ec854748
jseo42 <jayseo.js@gmail.com>
2023-09-28 10:25:37 -0500

    mobile mockup

--- index.js
+++ index.js
@@ -2,13 +2,39 @@ document.addEventListener("DOMContentLoaded", () => {
   let currentDate = new Date().toISOString().slice(0, 10);
   const date = document.getElementById('date')
   date.value = currentDate;;
-  
-
-  const form = document.getElementById('reservationForm')
-  form.addEventListener('submit', (e) => {
-    e.preventDefault;
-    console.log(e);
-  })
 });
+
+const reservation = document.getElementById('reservationDetails');
+const customer = document.getElementById('customerDetails');
+const success = document.getElementById('successMessage');
+const closeBtn = document.getElementById('closeButton')
+reservation.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (reservation.checkValidity()) {
+    reservation.classList.add('hidden');
+    customer.classList.remove('hidden');
+  } else {
+    alert('Please fill all required fields');
+  }
+});
+customer.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (customer.checkValidity()) {
+    customer.classList.add('hidden')
+    success.classList.remove('hidden');
+  } else {
+    alert('Please fill all required fields');
+  }
+});
+closeBtn.addEventListener('click', () => {
+  success.classList.add('hidden');
+  reservation.classList.remove('hidden');
+});
+function toggleNav() {
+  const fullscreenNav = document.getElementById('fullscreenNav');
+  const hamburgerIcon = document.getElementById('hamburger');
+  hamburgerIcon.classList.toggle("active");
+  fullscreenNav.style.display = fullscreenNav.style.display === "block" ? "none" : "block";
+}
 
 
